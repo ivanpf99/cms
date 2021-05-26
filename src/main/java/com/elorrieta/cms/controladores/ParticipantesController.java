77631498d7fb4bce2fd0c056210d1ea685d21651
cursos_externos <cursos_externos@HZ327390.elorrieta.int>
2021-05-26 13:58:31 +0200

    26

--- src/main/java/com/elorrieta/cms/controladores/ParticipantesController.java
+++ src/main/java/com/elorrieta/cms/controladores/ParticipantesController.java
@@ -27,17 +27,54 @@ public class ParticipantesController extends HttpServlet {
 			throws ServletException, IOException {
 
 		// recoger parametros, no tenemos
+		String paramId = request.getParameter("id");
+		String paramEliminar = request.getParameter("op");
 
-		// logica de negocio, conseguir coleccion de Participantes
+		if (paramId == null) { // listar
 
-		ArrayList<Participante> lista = ParticipanteDAO.getAll();
+			// logica de negocio, conseguir coleccion de Participantes
 
-		// enviar atributos para pintar
-		request.setAttribute("participantes", lista);
+			ArrayList<Participante> lista = ParticipanteDAO.getAll();
 
-		// ir a la vista
-		request.getRequestDispatcher("participantes.jsp?page=participantes").forward(request, response);
+			// enviar atributos para pintar
+			request.setAttribute("participantes", lista);
 
+			// ir a la vista
+			request.getRequestDispatcher("participantes.jsp?page=participantes").forward(request, response);
+
+		} else { // ir al formulario y mostrar datos
+
+			int id = Integer.parseInt(paramId);
+
+			if (paramEliminar != null) {
+
+				try {
+					ParticipanteDAO.delete(id);
+					request.setAttribute("mensajeTipo", "warning");
+					request.setAttribute("mensaje", "Eliminado Participante");
+				} catch (Exception e) {
+					request.setAttribute("mensajeTipo", "danger");
+					request.setAttribute("mensaje", "No se puede Eliminar");
+					e.printStackTrace();
+				}
+				ArrayList<Participante> lista = ParticipanteDAO.getAll();
+				request.setAttribute("participantes", lista);
+				request.getRequestDispatcher("participantes.jsp?page=participantes").forward(request, response);
+
+			} else {
+
+				Participante p = new Participante();
+
+				if (id > 0) { // ya existe, por lo cual lo recupero de la bbdd
+
+					p = ParticipanteDAO.getById(id);
+				}
+
+				request.setAttribute("participante", p);
+				request.getRequestDispatcher("formulario.jsp").forward(request, response);
+
+			}
+		}
 	}
 
 	/**
@@ -47,7 +84,36 @@ public class ParticipantesController extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		doGet(request, response);
+		int id = Integer.parseInt(request.getParameter("id"));
+		String nombre = request.getParameter("nombre");
+		String apellidos = request.getParameter("apellidos");
+		String email = request.getParameter("email");
+		String avatar = request.getParameter("avatar");
+
+		Participante p = new Participante();
+		p.setId(id);
+		p.setNombre(nombre);
+		p.setApellidos(apellidos);
+		p.setEmail(email);
+		p.setAvatar(avatar);
+
+		try {
+			if (id == 0) {
+				ParticipanteDAO.insert(p);
+			} else {
+				ParticipanteDAO.update(p);
+			}
+
+			request.setAttribute("mensaje", "Datos Guardados");
+
+		} catch (Exception e) {
+			request.setAttribute("mensaje", "El email esta repetido");
+
+		} finally {
+			request.setAttribute("participante", p);
+			request.getRequestDispatcher("formulario.jsp").forward(request, response);
+		}
+
 	}
 
 }
--- src/main/java/com/elorrieta/cms/modelo/dao/ParticipanteDAO.java
+++ src/main/java/com/elorrieta/cms/modelo/dao/ParticipanteDAO.java
@@ -24,8 +24,8 @@ public class ParticipanteDAO {
 	 */
 	public static ArrayList<Participante> getAll() {
 
-		ArrayList<Participante> coleccion = new ArrayList<Participante>();
-		String sql = "SELECT id, nombre, apellidos, email, avatar FROM participante ORDER BY id ASC; ";
+		ArrayList<Participante> lista = new ArrayList<Participante>();
+		String sql = "SELECT id, nombre, apellidos, email, avatar FROM participante; ";
 
 		try (
 
@@ -52,7 +52,7 @@ public class ParticipanteDAO {
 				p.setAvatar(rs.getString("avatar"));
 
 				// añadir objeto al ArrayList
-				coleccion.add(p);
+				lista.add(p);
 
 			}
 			// fin del bucle, ya no quedan mas lineas para leer
@@ -61,7 +61,123 @@ public class ParticipanteDAO {
 			e.printStackTrace();
 		}
 
-		return coleccion;
+		return lista;
+	}
+
+	/**
+	 * Buscamos un participante por su identificador
+	 * 
+	 * @param id int identificador del participantes
+	 * @return Participante con datos si encuentra, NULL si no lo encuentra
+	 */
+	public static Participante getById(int id) {
+
+		Participante p = null;
+		String sql = "SELECT id,nombre,apellidos,email, avatar FROM participante WHERE id = ?; ";
+
+		try (Connection con = ConnectionHelper.getConnection(); PreparedStatement pst = con.prepareStatement(sql)) {
+
+			pst.setInt(1, id);
+			try (ResultSet rs = pst.executeQuery()) {
+
+				while (rs.next()) { // hemos encontrado Participante por su ID
+
+					// cogemos los valres de las columnas
+					int colId = rs.getInt("id");
+					String colNombre = rs.getString("nombre");
+					String colApellidos = rs.getString("apellidos");
+					String colEmail = rs.getString("email");
+					String colAvatar = rs.getString("avatar");
+
+					p = new Participante();
+					p.setId(colId);
+					p.setNombre(colNombre);
+					p.setApellidos(colApellidos);
+					p.setEmail(colEmail);
+					p.setAvatar(colAvatar);
+				}
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return p;
+	}
+
+	/**
+	 * Elimina un particpante por su identificador
+	 * 
+	 * @param id int identificador
+	 * @return true si elimina, false en caso contrario
+	 * @throws Exception no deberia lanzar
+	 */
+	public static boolean delete(int id) throws Exception {
+
+		boolean eliminado = false;
+		String sql = "DELETE FROM participante WHERE id = ?;";
+		try (Connection con = ConnectionHelper.getConnection(); PreparedStatement pst = con.prepareStatement(sql)) {
+
+			pst.setInt(1, id);
+			if (pst.executeUpdate() == 1) {
+				eliminado = true;
+			}
+
+		}
+		return eliminado;
+	}
+
+	/**
+	 * Inserta un nuevo Participante
+	 * 
+	 * @param p Participante con los datos nuevos a insertar
+	 * @return true si lo inserta, false en caso contrario
+	 * @throws Exception si el email ya existe
+	 */
+	public static boolean insert(Participante p) throws Exception {
+		boolean insertado = false;
+		String sql = "INSERT INTO participante (nombre,apellidos,email,avatar) VALUES (?,?,?,?); ";
+		try (Connection con = ConnectionHelper.getConnection(); PreparedStatement pst = con.prepareStatement(sql)) {
+
+			pst.setString(1, p.getNombre());
+			pst.setString(2, p.getApellidos());
+			pst.setString(3, p.getEmail());
+			pst.setString(4, p.getAvatar());
+
+			if (pst.executeUpdate() == 1) {
+				insertado = true;
+			}
+
+		}
+
+		return insertado;
+	}
+
+	/**
+	 * Modifica un Participante
+	 * 
+	 * @param p Participante con los datos a modificar, importante que tenga un id
+	 * @return true si modifica, false en caso contrario
+	 * @throws Exception si el email ya existe en la tabla
+	 */
+	public static boolean update(Participante p) throws Exception {
+		boolean modificado = false;
+		String sql = "UPDATE participante SET nombre = ? ,apellidos = ?, email = ?, avatar = ?	WHERE id = ?;";
+		try (Connection con = ConnectionHelper.getConnection(); PreparedStatement pst = con.prepareStatement(sql)) {
+
+			pst.setString(1, p.getNombre());
+			pst.setString(2, p.getApellidos());
+			pst.setString(3, p.getEmail());
+			pst.setString(4, p.getAvatar());
+			pst.setInt(5, p.getId());
+
+			if (pst.executeUpdate() == 1) {
+				modificado = true;
+			}
+
+		}
+
+		return modificado;
 	}
 
 }
